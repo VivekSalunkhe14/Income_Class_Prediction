c39254eef5ec2970d7b89f9c6a59897c5035087b
VivekSalunkhe <viveksalunkhe80@gmail.com>
2021-04-17 16:27:34 +0530

    get updated from index

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ model = pickle.load(open("xgb_model.pkl", "rb"))
 print("Model Loaded")
 
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def home():
     return render_template("index.html")
 
